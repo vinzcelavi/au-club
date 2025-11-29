602ecfef460d058bbc15532fc55cb25465dddce5
Vincent Bianciotto <vincent@celavi.fr>
2025-11-29 09:12:27 +0100

    Refine Google review

--- components/google-reviews-section.tsx
+++ components/google-reviews-section.tsx
@@ -6,7 +6,7 @@ import { cn } from '@/lib/utils';
 
 const GoogleReviewsSection = () => {
   return (
-    <div className="w-full max-w-2xl mx-auto px-4 mt-2">
+    <div className="w-auto max-w-2xl mx-auto px-4 mt-2">
       <a
         href="https://share.google/F3SDjWVN8TDsWrEGB"
         target="_blank"
@@ -14,9 +14,9 @@ const GoogleReviewsSection = () => {
         aria-label="Voir les avis Google"
         className={cn(
           // Flexbox
-          'flex flex-col items-center justify-center gap-2',
+          'inline-flex flex-col items-center justify-center gap-2',
           // Spacing & Width
-          'p-3 w-[180px] mx-auto',
+          'p-3 w-auto mx-auto',
           // Border Radius
           'rounded-md',
           // Background
@@ -27,19 +27,26 @@ const GoogleReviewsSection = () => {
           'transition-all duration-300'
         )}
       >
-        <Image src="/google-favicon-2025.svg" alt="Google Favicon" width={16} height={16} className="block w-5 h-5" />
-
-        {/* Stars */}
-        <div className="flex justify-center gap-0.5 mt-1">
-          {[...Array(5)].map((_, index) => (
-            // biome-ignore lint/suspicious/noArrayIndexKey: <we need a stable key for the children>
-            <Star key={index} className="w-5 h-5 text-amber-400" />
-          ))}
+        <div className="flex items-center justify-center gap-2">
+          <Image
+            src="/google-favicon-2025.svg"
+            alt="Google Favicon"
+            width={18}
+            height={18}
+            className="block w-4.5 h-4.5"
+          />
+          {/* Stars */}
+          <div className="flex justify-center gap-0.5 mt-0.5">
+            {[...Array(5)].map((_, index) => (
+              // biome-ignore lint/suspicious/noArrayIndexKey: <we need a stable key for the children>
+              <Star key={index} className="w-5 h-5 text-amber-400" />
+            ))}
+          </div>
         </div>
 
         <div className="flex items-center justify-center gap-2">
-          <span className="text-white text-xs font-medium">
-            Note de <strong>4.9</strong> sur <strong>75</strong> avis
+          <span className="text-white text-xs font-normal">
+            Note de <strong>4.9</strong> sur <strong>78</strong> avis
           </span>
         </div>
       </a>
