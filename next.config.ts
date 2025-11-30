117c4bba9a7187f3446c035c5196a85ca2a640b1
Vincent Bianciotto <vincent@celavi.fr>
2025-11-30 08:44:33 +0100

    Remove redirections

--- next.config.ts
+++ next.config.ts
@@ -8,40 +8,6 @@ const nextConfig: NextConfig = {
         hostname: 'auclub.s3.eu-north-1.amazonaws.com'
       }
     ]
-  },
-  async redirects() {
-    return [
-      {
-        source: '/squash-et-lieux-incroyables/',
-        destination: '/',
-        permanent: true
-      },
-      {
-        source: '/squash-et-celebrites-dillustres-mordus-de-squash/',
-        destination: '/',
-        permanent: true
-      },
-      {
-        source: '/lhistoire-farfelue-du-squash/',
-        destination: '/',
-        permanent: true
-      },
-      {
-        source: '/allez-on-y-va/',
-        destination: '/',
-        permanent: true
-      },
-      {
-        source: '/le-squash-aux-jeux-olympiques-2028/',
-        destination: '/',
-        permanent: true
-      },
-      {
-        source: '/lhymne-du-club-a-la-une-du-site-de-la-fede/',
-        destination: '/',
-        permanent: true
-      }
-    ];
   }
 };
 
